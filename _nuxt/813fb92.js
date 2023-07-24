523d12a72042bc1c3271c7f10de6609caeafc7e8
Cheikh Gueye <bayecheikhgueye2@gmail.com>
2023-07-24 16:50:01 +0000

    first commit

---
+++ _nuxt/813fb92.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[249],{1867:function(t,e,n){"use strict";n.r(e);var o={mounted:function(){this.$isLogged()?this.$router.push("/dashboard"):this.$router.push("/login")}},r=n(108),component=Object(r.a)(o,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,"02ce0332",null);e.default=component.exports}}]);
